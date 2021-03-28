b959f63381e0d7b05faf686f2918120995878433
Conniesy2021 <77601635+Conniesy2021@users.noreply.github.com>
2021-03-28 17:49:11 -0500

    Update
    Update

--- activity6.py
+++ activity6.py
@@ -1,45 +1,46 @@
-import urllib.parse
+#Date: 26/3/2021
+#Class: ISQA3900
+#Purpose:Learn to use API and JSON
+#Name: Siyu Pan
+
+from datetime import datetime
 import requests
 import pytemperature
+now = datetime.now()
 
-
-def check_current_conditions(json_data):
-
+print("ISQA 3900 Open Weather API")
+print(now.strftime('%A, %B %d, %Y'))
+print()
+choice = "y"
+while choice.lower() == "y":
     try:
-        current_conditions = json_data['weather'][0]['description']
-        return current_conditions
-
-    except Exception:
-        str = 'Please enter valid input'
-        return str
-
-
-def current_temperature(json_data):
-    pass
-    
-def current_humidity(json_data):
-    pass
-    
-def low_temperature(json_data):
-    pass
-   
-def high_temperature(json_data):
-    pass
-   
-if __name__ == '__main__':
-
-    choice = 'y'
-    while choice.lower() == 'y':
-        print("ISQA 4900 Open Weather API")
-        city = input('Enter city: \t')
-        print('Use ISO letter country code like: https://countrycode.org/')
-        country = input('Enter the country code: \t')
+        city = input("Enter city: ")
+        print("Use ISO letter country code like: https://countrycode.org/")
+        country = input("Enter country code: ")
         api_start = 'https://api.openweathermap.org/data/2.5/weather?q='
-        api_key = '&appid=YOUR API KEY HERE !!!'
+        api_key = '&appid=bb5ca18932706e1b9c7ef4baec4c64f1'
         url = api_start + city + ',' + country + api_key
         json_data = requests.get(url).json()
-        current_conditions = check_current_conditions(json_data)
-        print('Current conditions: '+ current_conditions)
-        choice = input("Continue(y/n)?")
+        weather_description = json_data['weather'][0]['description']
+        print("Current conditions: ", weather_description)
+        weather_temperature = pytemperature.k2f(json_data['main']['temp'])
+        print("Current Temperature in Fahrenheit: ", weather_temperature)
+        current_pressure = json_data['main']['pressure']
+        print("Current Pressure in hpa: ", current_pressure)
+        current_hum = json_data['main']['humidity']
+        print("Current Humidity: ", current_hum, end='')
+        print("%")
+        low_temp = pytemperature.k2f(json_data['main']['temp_min'])
+        print("Expected Low Temperature in Fahrenheit: ", low_temp)
+        high_temp = pytemperature.k2f(json_data['main']['temp_max'])
+        print("Expected High Temperature in Fahrenheit: ", high_temp)
+
+    except KeyError:
+        print("     Unable to access {} in {}".format(city,country))
+        print("     Verify city name and country code")
+
+    finally:
+        choice = input("Continue (y/n)?: ")
         print()
-        print("Bye!")
+
+print('Bye')
